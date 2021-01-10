ac2c7f176bdb5be3b319f183dfa40324d22ddcbd
chhetri28 <chettrisourav28@gmail.com>
2021-01-11 01:34:44 +0530

    Added few new functions

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,12 @@
-//4. Create a Footer.jsx component that renders a <footer> element
-//to show a copyright message in a <p> with a dynamically updated year.
 import React from "react";
+
 function Footer() {
-    const curryr = new Date().getFullYear();
-    return (
-        <footer>
-            <p> Copyright© RED {curryr} 
-            </p>
-       </footer> 
-    );
+  const year = new Date().getFullYear();
+  return (
+    <footer>
+      <p>Copyright ⓒ {year}</p>
+    </footer>
+  );
 }
+
 export default Footer;
